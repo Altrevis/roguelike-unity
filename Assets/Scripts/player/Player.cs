bf7817699f4d4c466870459bbd120b7dc96fd913
Alexandre <alexandre.queudeville@ynov.com>
2025-02-06 17:12:42 +0100

    movement warrior

--- Assets/Scripts/player/Player.cs
+++ Assets/Scripts/player/Player.cs
@@ -1,27 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public GameObject magePrefab;
-    public GameObject warriorPrefab;
+    [SerializeField] private float speed = 3f;
+    private bool isGrounded = false;
 
-    private GameObject currentPlayer;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
-    void Start()
+    private void Awake()
     {
-        SelectClass();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    
+    private void FixedUpdate()
+    {
+        CheckGround();
     }
 
-    void SelectClass()
+    private void Update()
+{
+    if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
     {
-        if (PlayerPrefs.GetString("SelectedClass") == "Mage")
-        {
-            currentPlayer = Instantiate(magePrefab, transform.position, transform.rotation);
-        }
-        else
-        {
-            currentPlayer = Instantiate(warriorPrefab, transform.position, transform.rotation);
-        }
+        Run();
+    }
+}
+
+    private void Run()
+{
+    Vector3 dir = Vector3.zero;
+
+    if (Input.GetKey(KeyCode.W))
+    {
+        dir += Vector3.up;
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+        dir += Vector3.down;
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+        dir += Vector3.left;
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+        dir += Vector3.right;
+    }
+
+    dir = dir.normalized;
+
+    transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+
+    sprite.flipX = dir.x < 0.0f;
+}
+
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
     }
 }
